c9437653a655e0911af4578b5b7bcc23d6cb871d
n._.d_hx <145911265+ndh12@users.noreply.github.com>
2026-02-12 10:26:31 +0900

    Configure for deployment and add source code

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/01/">
             <AuthProvider>
                 <App />
             </AuthProvider>
